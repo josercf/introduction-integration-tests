2ca2b05b278ad2b29c68af017db3eccc327294c2
José Romualdo <joseromualdo@outlook.com>
2023-09-14 08:40:04 -0300

    finishing unit tests

--- index.test.js
+++ index.test.js
@@ -3,3 +3,8 @@ test('Aplicar desconto', () => {
     const result = index.aplicarDesconto(10,5);
     expect(result).toEqual(5);
 })
+
+test('Aplicar desconto grande', () => {
+    const result = index.aplicarDesconto(5,10);
+    expect(result).toEqual(0);
+});
